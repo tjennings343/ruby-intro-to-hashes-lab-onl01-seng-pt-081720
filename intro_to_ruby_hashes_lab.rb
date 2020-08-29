502c0a377c3c458273c2e7ead8161bda23c3030e
Trey Jennings <tjennings343@gmail.com>
2020-08-29 03:03:06 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,17 +12,12 @@ def monopoly
 end
 
 def monopoly_with_second_tier
-monopoly = {:railroads => {
-  :pieces => 4}
-}
+monopoly = {:railroads => {:pieces => 4}}
 
 end
 
 def monopoly_with_third_tier
-  monopoly = {:railroads =>{
-  :pieces => 4,}
-  :names => {}
-  }
+  monopoly = {:railroads =>{:pieces => 4,}}
 
 end
 
