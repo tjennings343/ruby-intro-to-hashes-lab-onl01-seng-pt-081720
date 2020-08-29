1c2a4892be274a5f40ae41b52d8488a4aa86493e
Trey Jennings <tjennings343@gmail.com>
2020-08-29 03:07:20 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,6 +17,7 @@ monopoly = {:railroads => {:pieces => 4, :names => {}, :rent_in_dollars => {}}}
 end
 
 def monopoly_with_third_tier
+  monopoly = {:railroads => {:pieces => 4, :names => {}, :rent_in_dollars => {:one_piece_owned => 25}}}
 
 
 end
